29eac056f7f0d25d7822a3462b902c71a26ba56e
Md Wahiduzzaman Suva <wshuvo360@gmail.com>
2024-12-31 04:55:17 +0600

    null commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import googleLogo from "@/public/google.png";
 import githubLogo from "@/public/github.png";
 import {
   CredentialsSignInButton,
-  GithubSignInButton,
   GoogleSignInButton,
 } from "@/components/authButtons";
 import { getServerSession } from "next-auth";
@@ -24,7 +23,6 @@ export default async function SignInPage() {
       <div className="flex flex-col items-center mt-10 p-10 shadow-md">
         <h1 className="mt-10 mb-4 text-4xl font-bold">Sign In</h1>
         <GoogleSignInButton />
-        <GithubSignInButton />
         <span className="text-2xl font-semibold text-white text-center mt-8">
           Or
         </span>
