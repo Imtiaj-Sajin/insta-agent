5964ceb680477238059fba56344b5c5847d6b617
Imtiaj Sajin <imtiajsajin@gmail.com>
2025-01-03 14:19:06 +0600

    signup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import TwitterNavbar from "@/components/twitterNavbar";
+// import TwitterNavbar from "@/components/twitterNavbar";
 // import "../styles/globals.css";
 import { Inter } from "next/font/google";
 import { NextAuthProvider } from "./providers";
