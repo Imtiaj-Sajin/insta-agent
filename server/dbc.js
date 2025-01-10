e9b6573ae240efb18e1f5763f18e29cca35d7573
Md Wahiduzzaman Suva <wshuvo360@gmail.com>
2025-01-10 15:14:57 +0600

    error or dbc.js

--- server/dbc.js
+++ server/dbc.js
@@ -7,14 +7,27 @@ const pool = mysql.createPool({
   database: process.env.DATABASE_NAME,  
   port: 3306,
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  typeCast: function (field, next) {
-    if (field.type === 'NEWDECIMAL' || field.type === 'BIGINT') {
-      return field.string(); // Return as string to prevent precision loss
-    }
-    return next();
-  },
+  // connectionLimit: 10,
+  // queueLimit: 0,
+  // typeCast: function (field, next) {
+  //   if (field.type === 'NEWDECIMAL' || field.type === 'BIGINT') {
+  //     return field.string(); // Return as string to prevent precision loss
+  //   }
+  //   return next();
+  // },
 });
-
+(async () => {
+  try {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        console.error('Database connection failed:', err.message);
+      } else {
+        console.log('Connected to MySQL database successfully!');
+        connection.release();
+      }
+    });
+  } catch (error) {
+    console.error('Database connection failed:', error.message);
+  }
+})();
 module.exports = pool.promise();
